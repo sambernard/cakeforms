775aeb041f36b44b455d400ff07518f89f9d8b0e
Sam Bernard <sam@sambernard.net>
2010-05-20 11:36:11 -0500

    Moved form creation to helper.

---
+++ views/helpers/cform.php
@@ -0,0 +1,77 @@
+<?php  
+class CformHelper extends AppHelper { 
+    public $helpers = array('Html', 'Form');
+    public $openFieldset = false;
+    
+    function insert($formData){
+        $out = '';
+
+        if(!empty($formData['Cform'])){
+            $out .= $this->Form->create('Submission', array('url' => '/' . $this->params['url']['url']));
+            $out .= $this->Form->hidden('Cform.id', array('value' => $formData['Cform']['id']));
+            $out .= $this->Form->hidden('Cform.submitHere', array('value' => true));
+            
+            if(isset($formData['FormField'])){
+                foreach($formData['FormField'] as $field){
+                    $out .= $this->field($field);
+                }
+            }
+        }
+        
+        if($this->openFieldset == true){
+                $out .= "</fieldset>";
+        }
+        
+        $out .= $this->Form->end('Submit');        
+        
+        return $this->output($out);
+    }
+    
+    function field($field, $custom_options = array()){
+        $options = array();
+        $out = '';
+        
+        if(!empty($field['type'])){
+                if($field['type'] == 'fieldset'){
+                        if($this->openFieldset == true){
+                                $out .= "</fieldset>";
+                        }
+                        
+                        $out .=  "<fieldset>";
+                        $this->openFieldset = true;
+                        
+                        if(!empty($field['name'])){
+                                $out .= "<legend>".Inflector::humanize($field['name'])."</legend>";
+                                $out .= $this->Form->hidden('Cform.fs_' . $field['name'], array('value' => $field['name']));
+                        }
+                        
+                } else {
+                        $options['type'] = $field['type'];
+                        if(in_array($field['type'], array('select', 'checkbox', 'radio'))){
+                                if($field['type'] == 'checkbox' && !empty($field['options'])){
+                                        $options['type'] = 'select';
+                                        $options['multiple'] = 'checkbox';
+                                }
+                                
+                                $options['options'] = $field['options'];
+                        }
+                        
+                        if(!empty($field['label'])){
+                                $options['label'] = $field['label'];
+                        }
+                        
+                        if(!empty($field['default']) && empty($this->data['Cform'][$field['name']])){
+                                $options['value'] = $field['default'];
+                        }
+                        
+                        $options = Set::merge($custom_options, $options);
+                        
+                        $out .= $this->Form->input('Cform.' . $field['name'], $options);
+                
+                }
+        }
+        return $out;
+    }
+} 
+
+?>
