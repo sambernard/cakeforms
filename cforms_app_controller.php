c9a8ee34ea54d72fb5e1a6ed930675300a7feb33
Sam Bernard <sam@sambernard.net>
2010-05-20 11:37:52 -0500

    Add Auth check/loginaction

--- cforms_app_controller.php
+++ cforms_app_controller.php
@@ -9,6 +9,10 @@ class CformsAppController extends AppController {
         if($this->RequestHandler->isAjax()){
             Configure::write('debug', 0);
         }
+        
+        if(isset($this->Auth)){
+            $this->Auth->loginAction = array('controller' => 'users', 'action' => 'login', 'admin' => false, 'plugin' => null);
+        }
     }
 }
 
